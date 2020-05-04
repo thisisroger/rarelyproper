2e201647b1ce59accd6dbe6555d4e3b6212c4664
Roger Flanagan <rogerf@zoosk.com>
2020-05-04 12:01:34 -0700

    Adding Celebrate Some Failure

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ app.use(express.static(__dirname + '/public'));
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/',function(req,res) {
-    res.sendFile(__dirname + '/index.html');
-  });
+  res.sendFile(__dirname + '/index.html');
+});
 
-  app.get('/cv',function(req,res) {
-    res.sendFile(__dirname + '/cv.html');
-  });
+app.get('/cv',function(req,res) {
+  res.sendFile(__dirname + '/cv.html');
+});
+
+app.get('/celebratesomefailure',function(req,res) {
+  res.sendFile(__dirname + '/celebrate-some-failure/index.html');
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
